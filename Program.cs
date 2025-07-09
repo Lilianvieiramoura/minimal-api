e860b679fe6113a034a56b53e4b76b952e27e579
Lílian Vieira <lilianv528@gmail.com>
2025-07-09 15:06:24 -0300

    login

--- Program.cs
+++ Program.cs
@@ -1,17 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using MinimalApi.Dominio.Entidades;
+using MinimalApi.Dominio.Interfaces;
+using MinimalApi.Dominio.Servicos;
 using MinimalApi.DTOs;
+using MinimalApi.Infraestrutura.Db;
 
 var builder = WebApplication.CreateBuilder(args);
 builder.WebHost.UseUrls("http://localhost:5000");
+
+builder.Services.AddScoped<IAdministradorServico, AdministradorServico>();
+
+builder.Services.AddDbContext<DbContexto>(options =>
+{
+  options.UseMySql(
+    builder.Configuration.GetConnectionString("mysql"),
+    ServerVersion.AutoDetect(builder.Configuration.GetConnectionString("mysql"))
+  );
+});
+
 var app = builder.Build();
 
 app.MapGet("/", () => "Hello World!");
 
-app.MapPost("/login", (MinimalApi.DTOs.LoginDTO loginDTO) =>
+app.MapPost("/login", ([FromBody] LoginDTO loginDTO, IAdministradorServico administradorServico) =>
 {
-  if (loginDTO.Email == "adm@teste.com" && loginDTO.Senha == "123456")
+  if (administradorServico.Login(loginDTO) != null)
     return Results.Ok("Login com sucesso");
   else
-
     return Results.Unauthorized();
 });
 
